12bf7161f689a2a310cb22e9b0a0d8201fc8273a
Sam Hudock <shudock@altair.com>
2020-09-18 17:49:22 -0400

    Adds batch extension installer/uninstaller

---
+++ extensions/VSCode_Install_Extensions.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+ECHO Press ANY KEY to install prefered VSCode extensions with the 'code' command!
+PAUSE
+ECHO == Started installing...
+code --install-extension alefragnani.bookmarks >> code --install-extension ms-dotnettools.csharp >> code --install-extension naumovs.color-highlight >> code --install-extension ferrierbenjamin.fold-unfold-all-icone >> code --install-extension dimitarnonov.jellybeans-theme >> code --install-extension alefragnani.project-manager >> code --install-extension efbenson.scad >> code --install-extension wayou.vscode-todo-highlight >> code --install-extension shardulm94.trailing-spaces >> code --install-extension vscode-icons-team.vscode-icons >> code --install-extension dotjoshjohnson.xml
+ECHO == ALL DONE!
+PAUSE
---
+++ extensions/VSCode_Uninstall_Extensions.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+ECHO Press ANY KEY to uninstall prefered VSCode extensions with the 'code' command!
+PAUSE
+ECHO == Started uninstalling...
+code --uninstall-extension alefragnani.bookmarks >> code --uninstall-extension ms-dotnettools.csharp >> code --uninstall-extension naumovs.color-highlight >> code --uninstall-extension ferrierbenjamin.fold-unfold-all-icone >> code --uninstall-extension dimitarnonov.jellybeans-theme >> code --uninstall-extension alefragnani.project-manager >> code --uninstall-extension efbenson.scad >> code --uninstall-extension wayou.vscode-todo-highlight >> code --uninstall-extension shardulm94.trailing-spaces >> code --uninstall-extension vscode-icons-team.vscode-icons >> code --uninstall-extension dotjoshjohnson.xml
+ECHO == ALL DONE!
+PAUSE
