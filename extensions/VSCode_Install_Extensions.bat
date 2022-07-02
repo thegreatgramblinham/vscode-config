f762c8800d1dd5bdcf3591ce34b1f565f3c2d228
Sam Hudock <samhudock@gmail.com>
2022-07-02 15:13:53 -0400

    Updates the extensions list and install/uninstall files with changes

--- extensions/VSCode_Install_Extensions.bat
+++ extensions/VSCode_Install_Extensions.bat
@@ -2,6 +2,6 @@
 ECHO Press ANY KEY to install prefered VSCode extensions with the 'code' command!
 PAUSE
 ECHO == Started installing...
-code --install-extension alefragnani.bookmarks >> code --install-extension ms-dotnettools.csharp >> code --install-extension naumovs.color-highlight >> code --install-extension ferrierbenjamin.fold-unfold-all-icone >> code --install-extension dimitarnonov.jellybeans-theme >> code --install-extension alefragnani.project-manager >> code --install-extension efbenson.scad >> code --install-extension wayou.vscode-todo-highlight >> code --install-extension shardulm94.trailing-spaces >> code --install-extension vscode-icons-team.vscode-icons >> code --install-extension dotjoshjohnson.xml
+code --install-extension alefragnani.bookmarks >> code --install-extension ms-dotnettools.csharp >> code --install-extension naumovs.color-highlight >> code --install-extension ferrierbenjamin.fold-unfold-all-icone >> code --install-extension dimitarnonov.jellybeans-theme >> code --install-extension alefragnani.project-manager >> code --install-extension efbenson.scad >> code --install-extension jgclark.vscode-todo-highlight >> code --install-extension shardulm94.trailing-spaces >> code --install-extension vscode-icons-team.vscode-icons >> code --install-extension dotjoshjohnson.xml >> code --install-extension ms-vscode.cpptools
 ECHO == ALL DONE!
 PAUSE
