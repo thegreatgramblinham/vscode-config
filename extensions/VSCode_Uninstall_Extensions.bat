f4fcf77edfb4f7750c6309c1123b15e48c542461
Sam Hudock <shudock@altair.com>
2022-12-14 11:22:13 -0500

    Removes C# extension, migrates openScad extension

--- extensions/VSCode_Uninstall_Extensions.bat
+++ extensions/VSCode_Uninstall_Extensions.bat
@@ -2,6 +2,7 @@
 ECHO Press ANY KEY to uninstall prefered VSCode extensions with the 'code' command!
 PAUSE
 ECHO == Started uninstalling...
-code --uninstall-extension alefragnani.bookmarks >> code --uninstall-extension ms-dotnettools.csharp >> code --uninstall-extension naumovs.color-highlight >> code --uninstall-extension ferrierbenjamin.fold-unfold-all-icone >> code --uninstall-extension dimitarnonov.jellybeans-theme >> code --uninstall-extension alefragnani.project-manager >> code --uninstall-extension efbenson.scad >> code --uninstall-extension jgclark.vscode-todo-highlight >> code --uninstall-extension shardulm94.trailing-spaces >> code --uninstall-extension vscode-icons-team.vscode-icons >> code --uninstall-extension dotjoshjohnson.xml >> code --uninstall-extension ms-vscode.cpptools
+code --uninstall-extension dimitarnonov.jellybeans-theme >> code --uninstall-extension alefragnani.bookmarks >> code --uninstall-extension ms-vscode.cpptools >> code --uninstall-extension naumovs.color-highlight >> code --uninstall-extension ferrierbenjamin.fold-unfold-all-icone >>
+code --uninstall-extension donjayamanne.githistory >> code --uninstall-extension alefragnani.project-manager >> code --uninstall-extension leathong.openscad-language-support >> code --uninstall-extension jgclark.vscode-todo-highlight >> code --uninstall-extension shardulm94.trailing-spaces >> code --uninstall-extension vscode-icons-team.vscode-icons >> code --uninstall-extension dotjoshjohnson.xml
 ECHO == ALL DONE!
 PAUSE
